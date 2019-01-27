a0382fba4f45cb842468145969cca46b120fe108
Mai-Pu <619345817@qq.com>
2019-01-27 23:54:21 +0800

    加入搜索结果显示

--- pages/search/search.js
+++ pages/search/search.js
@@ -8,79 +8,88 @@ Page({
   data: {
 
   },
-  loadData:function(url){
+  loadData: function(url) {
     var that = this;
     wx.request({
       url: url,
-      method:'GET',
-      success:function(res){
+      method: 'GET',
+      success: function(res) {
         var loaddata = res.data.showapi_res_body.pagebean.contentlist;
         that.setData({
-          tourdata:loaddata});
-        console.log(that.data);
+          tourdata: loaddata
+        })
       },
-      fail:function(error){
+      fail: function(error) {
         console.log(2)
       }
     })
   },
-  searchData:function(e){
+  searchData: function(e) {
     var text = e.detail.value;
-    this.loadData(baseurl + '&keyword=' + text);
+    if (text != '')
+      this.loadData(baseurl + '&keyword=' + text);
+    else
+      this.setData({
+        tourdata: {}
+      });
+  },
+  tapAnswer: function(event) {
+    var name = event.currentTarget.dataset.name;
+    console.log(name);
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
